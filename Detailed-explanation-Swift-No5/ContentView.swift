cd11a646813e9e78c162117b2b0eb75d7f9a12c1
xxminamixx <pencil3215@gmial.com>
2019-12-01 11:39:01 +0900

    Initial Commit

---
+++ Detailed-explanation-Swift-No5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Detailed-explanation-Swift-No5
+//
+//  Created by 南京兵 on 2019/12/01.
+//  Copyright © 2019 南京兵. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Detailed-explanation-Swift-No5Tests/Detailed_explanation_Swift_No5Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Detailed_explanation_Swift_No5Tests.swift
+//  Detailed-explanation-Swift-No5Tests
+//
+//  Created by 南京兵 on 2019/12/01.
+//  Copyright © 2019 南京兵. All rights reserved.
+//
+
+import XCTest
+@testable import Detailed_explanation_Swift_No5
+
+class Detailed_explanation_Swift_No5Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
